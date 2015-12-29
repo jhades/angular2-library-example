50ec7dc7b611962825726d6b936cf2346df7c0c5
Your Name <you@example.com>
2015-12-29 14:25:37 +0000

    adding jspm-based example

--- example/src/App.ts
+++ example/src/App.ts
@@ -1,15 +1,20 @@
 
-import 'reflect-metadata';
+//import 'reflect-metadata';
 import {Component} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
-//import {HelloWorld} from 'angular2-library-example/lib/components/HelloWorld';
+
+import {HelloWorld} from 'angular2-library-example';
 //import {HelloWorld} from './HelloWorld';
-import {HelloWorld} from '../jspm_packages/npm/angular2-library-example@1.0.2/lib/components/HelloWorld';
+//import {HelloWorld} from '../jspm_packages/npm/angular2-library-example@1.0.2/lib/components/HelloWorld';
+
+import {TranslateService, TranslatePipe} from 'ng2-translate/ng2-translate';
+
 
 
 @Component({
     selector: 'app',
     directives: [HelloWorld],
+    pipes: [TranslatePipe],
     template: `<input placeholder="Type Hello World!" (keyup)="onKeyUp(input)" #input>
                {{message}}
                <hello-world></hello-world>
@@ -25,4 +30,4 @@ export class App {
 
 }
 
-bootstrap(App);
+bootstrap(App,[TranslateService]);
