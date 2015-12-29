26081a8e6312b4b1860fc21db61f26f19e1208da
Your Name <you@example.com>
2015-12-29 19:52:44 +0000

    adding jspm-based example

--- examples/systemjs-only/src/App.ts
+++ examples/systemjs-only/src/App.ts
@@ -1,17 +1,12 @@
 
-//import 'reflect-metadata';
 import {Component} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
-
 import {HelloWorld} from 'angular2-library-example/components';
-import {TranslateService, TranslatePipe} from 'ng2-translate/ng2-translate';
-
 
 
 @Component({
     selector: 'app',
     directives: [HelloWorld],
-    pipes: [TranslatePipe],
     template: `<input placeholder="Type Hello World!" (keyup)="onKeyUp(input)" #input>
                {{message}}
                <hello-world></hello-world>
@@ -27,4 +22,4 @@ export class App {
 
 }
 
-bootstrap(App,[TranslateService]);
+bootstrap(App);
