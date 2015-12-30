95f465ac63713953a1dc6127ce7f26e12ceef0bd
Your Name <you@example.com>
2015-12-30 22:40:23 +0000

    css best practices

--- src/HelloWorld.ts
+++ src/HelloWorld.ts
@@ -1,8 +1,15 @@
 
 import {Component} from 'angular2/core';
+import {ViewEncapsulation} from 'angular2/core';
 
 @Component({
     selector: 'hello-world',
+    encapsulation: ViewEncapsulation.Emulated,
+    styles: [`
+       h1 {
+            color: blue;
+        }
+    `],
     template: `<h1 (click)="onClick()">{{message}}</h1>`
 })
 export class HelloWorld {
